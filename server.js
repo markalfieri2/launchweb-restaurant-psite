ab19c7da19fbc8d9c25be595a5e6e3fb8e222fd6
markalfieri2 <markalfieri@usf.edu>
2025-08-03 16:44:22 -0400

    Fixed ejs paths

--- server.js
+++ server.js
@@ -47,3 +47,8 @@ app.post('/create-checkout-session', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
